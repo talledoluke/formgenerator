662013570af3d837fcfb5104b8b011553b6ce24d
talledoluke <124649619+talledoluke@users.noreply.github.com>
2023-05-09 21:34:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 const insert = () => {
-    let type = document.querySelector('#type').value 
-    let label = document.querySelector('#label').value 
+  let type = document.querySelector('#type').value 
+  let label = document.querySelector('#label').value 
 
-    switch(type){
-        case 'text': appendText(label)
-            break
-        case 'button': appendButton(label)
-            break	
-		case 'radio': appendRadio(label)
-			break
-    }
+  switch(type){
+    case 'text': 
+      appendText(label)
+      break
+    case 'button': 
+      appendButton(label)
+      break
+
+  }
 }
 
 const appendText = (label = "No label") => {
-    let el = document.createElement('input')
+  let el = document.createElement('input')
 
-    el.setAttribute('type', 'text')
-    el.setAttribute('class', 'form-control mt-2')
-    el.setAttribute('placeholder', label)
+  el.setAttribute('type', 'text')
+  el.setAttribute('class', 'form-control mt-2')
+  el.setAttribute('placeholder', label)
 
-    document.querySelector('#display').append(el)
-    
+  document.querySelector('#display').append(el) 
 }
 
 const appendButton = (label = "No label") => {
-    let el = document.createElement('button')
+  let el = document.createElement('button')
 
-    el.setAttribute('type', 'text')
-    el.setAttribute('class', 'btn btn-primary mt-2') 
-    el.textContent = label
+  el.setAttribute('type', 'text')
+  el.setAttribute('class', 'btn btn-primary mt-2') 
+  el.textContent = label
 
-    document.querySelector('#display').append(el)
+  document.querySelector('#display').append(el)
 }
 
+const deleteElement = () => {
+  let deleteElement = document.querySelector('#display > *:last-child');
+  if (deleteElement) {
+    deleteElement.remove();
+  }
+}
